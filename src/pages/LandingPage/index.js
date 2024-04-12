1a69001341097e85ddc349703ce2a21fec4b5a79
Necmi Gunduz <necmigunduz@gmail.com>
2024-04-12 18:19:14 +0300

    add: logo

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -7,10 +7,18 @@ function LandingPage() {
     <div className='container'>
       <h3 className='header'>Welcome to Introvert vs. Extrovert Psychology Quiz</h3>
       <article>
-        <p className='text'>This page is prepared for a quiz to determine a personality is whether an extrovert or an introvert.
-        Several questions are to be asked, and your answers will be analyzed to understand whether you are more extrovert or introvert. 
-        Of course, this is a basic quiz prepared to develop a sense about your personality, and it is not considered as a scientific study.
-        </p>
+        <ul>
+          <li>
+            <p className='text'>This page is prepared for a quiz to determine a personality is whether an extrovert or an introvert.</p>
+          </li>
+          <li>
+            <p className='text'>Several questions are asked, and your answers will be analyzed to develop a sense whether you are more extrovert or introvert.</p>
+          </li>
+          <li>
+            <p className='text'>Of course, this is a basic quiz prepared to develop a sense about your personality, and it is not considered as a scientific study.</p>
+          </li>
+        </ul>
+
       </article>
       <Link to={'/quiz'} className='link'>Take the quiz</Link>
     </div>
