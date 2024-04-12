d7a55ce531780d973202f3302e0ae95e0322ca10
Necmi Gunduz <necmigunduz@gmail.com>
2024-04-12 08:59:03 +0300

    add: react with redux slice

---
+++ redux/slices/questionSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    questions: [],
+    questionIndex: 0,
+}
+
+const questionsSlice = createSlice({
+    name: 'questions',
+    initialState,
+    reducers: {
+        setQuestions: (state, action) => {
+            state.questions = action.payload
+        },
+        setQuestionIndex: (state, action) => {
+            state.questionIndex = action.payload
+        }
+    },
+    extraReducers: {},
+});
+
+export const {setQuestions, setQuestionIndex} = questionsSlice.actions;
+
+export default questionsSlice.reducer;
