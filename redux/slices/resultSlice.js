cdaf43c00d953153f40e1f2b11820be8cd41c90a
Necmi Gunduz <necmigunduz@gmail.com>
2024-04-12 09:08:35 +0300

    add: skeleton of the project

---
+++ redux/slices/resultSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    results: []
+};
+
+const resultSlice = createSlice({
+    name: 'resultSlice',
+    initialState,
+    reducers: {
+        setResults: (state, action) => {
+            state.results.push(action.payload)
+        },
+    },
+    extraReducers: {},
+});
+
+export const { setResults } = resultSlice.actions;
+
+export default resultSlice.reducer;
